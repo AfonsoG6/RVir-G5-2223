c66d0d73362a4f5eb4f3f9d12c090316516879e1
Afonso Gomes <afonsopamg@gmail.com>
2022-12-20 23:45:30 +0000

    Carolina changes

--- RVir [Project]/Assets/Scripts/WristUI.cs
+++ RVir [Project]/Assets/Scripts/WristUI.cs
@@ -5,26 +5,26 @@ using UnityEngine.InputSystem;
 
 public class WristUI : MonoBehaviour
 {
-    public InputActionAsset inputActions;
+	public InputActionAsset inputActions;
 
-    private Canvas wristUICanvas;
-    private InputAction menu;
+	private Canvas wristUICanvas;
+	private InputAction menu;
 
-    void Start()
-    {
-        wristUICanvas = GetComponent<Canvas>();
-        menu = inputActions.FindActionMap("XRI LeftHand").FindAction("Menu");
-        menu.Enable();
-        menu.performed += ToggleMenu;
-    }
+	void Start()
+	{
+		wristUICanvas = GetComponent<Canvas>();
+		menu = inputActions.FindActionMap("XRI LeftHand Interaction").FindAction("Menu");
+		menu.Enable();
+		menu.performed += ToggleMenu;
+	}
 
-    void OnDestroy()
-    {
-        menu.performed -= ToggleMenu;
-    }
+	void OnDestroy()
+	{
+		menu.performed -= ToggleMenu;
+	}
 
-    public void ToggleMenu(InputAction.CallbackContext ctx)
-    {
-        wristUICanvas.enabled = !wristUICanvas.enabled;
-    }
+	public void ToggleMenu(InputAction.CallbackContext ctx)
+	{
+		wristUICanvas.enabled = !wristUICanvas.enabled;
+	}
 }
