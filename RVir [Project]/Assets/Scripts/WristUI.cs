3f0863c90296a60ecb3b91c85b39a75bfebbc859
Afonso Gomes <afonsopamg@gmail.com>
2022-12-21 02:17:19 +0000

    UI working

--- RVir [Project]/Assets/Scripts/WristUI.cs
+++ RVir [Project]/Assets/Scripts/WristUI.cs
@@ -1,30 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class WristUI : MonoBehaviour
 {
 	public InputActionAsset inputActions;
-
 	private Canvas wristUICanvas;
-	private InputAction menu;
+	private InputAction menuInputAction;
+	// --------------------------------------------------------------------------------------------
+	private int sonarRadiusValue;
+	private Slider sonarRadiusSlider;
+	private TMP_Text sonarRadiusText;
+	// --------------------------------------------------------------------------------------------
+	private GameObject lightsObject;
+	// --------------------------------------------------------------------------------------------
+
 
 	void Start()
 	{
 		wristUICanvas = GetComponent<Canvas>();
-		menu = inputActions.FindActionMap("XRI LeftHand Interaction").FindAction("Menu");
-		menu.Enable();
-		menu.performed += ToggleMenu;
+		wristUICanvas.enabled = false;
+		menuInputAction = inputActions.FindActionMap("XRI LeftHand Interaction").FindAction("Menu");
+		menuInputAction.Enable();
+		menuInputAction.performed += ToggleMenu;
+
+		sonarRadiusSlider = GetComponentInChildren<Slider>();
+		sonarRadiusValue = (int)sonarRadiusSlider.value;
+
+		sonarRadiusText = sonarRadiusSlider.transform.GetChild(2).GetChild(0).GetChild(0).GetComponent<TMP_Text>();
+		sonarRadiusText.SetText(sonarRadiusValue.ToString());
+
+		lightsObject = GameObject.Find("Lighting");
 	}
 
 	void OnDestroy()
 	{
-		menu.performed -= ToggleMenu;
+		menuInputAction.performed -= ToggleMenu;
 	}
 
 	public void ToggleMenu(InputAction.CallbackContext ctx)
 	{
 		wristUICanvas.enabled = !wristUICanvas.enabled;
 	}
+
+	public void IncreaseSonarRadius()
+	{
+		if (sonarRadiusValue < 10)
+		{
+			sonarRadiusValue++;
+		}
+		sonarRadiusSlider.value = sonarRadiusValue;
+		sonarRadiusText.SetText(sonarRadiusValue.ToString());
+	}
+
+	public void DecreaseSonarRadius()
+	{
+		if (sonarRadiusValue > 0)
+		{
+			sonarRadiusValue--;
+		}
+		sonarRadiusSlider.value = sonarRadiusValue;
+		sonarRadiusText.SetText(sonarRadiusValue.ToString());
+	}
+
+	public void ToggleLights()
+	{
+		lightsObject.SetActive(!lightsObject.activeSelf);
+	}
+
 }
